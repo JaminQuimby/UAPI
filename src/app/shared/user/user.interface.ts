00b1e7e2a182d87d89f8052555a20fae9f97625e
Jamin Quimby <jaminquimby@hotmail.com>
2017-03-05 14:38:23 -0500

    adding user interface

---
+++ src/app/shared/user/user.interface.ts
@@ -0,0 +1,11 @@
+export interface UserInterface {
+    uid: string;
+    email: string;
+    photoURL: string;
+    displayName: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    title: string;
+    skype?: string;
+}
