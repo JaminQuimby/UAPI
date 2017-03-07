a72151aff04ee8217caf56af13a01fb8cac41a1e
Jamin Quimby <jaminquimby@hotmail.com>
2017-03-06 22:52:47 -0500

    firebase wire, https update

--- src/app/+e-commerce/e-commerce.module.ts
+++ src/app/+e-commerce/e-commerce.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders/orders.component';
 import { ProductsViewComponent } from './products-view/products-view.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import {uapiLayoutModule} from "../shared/layout/layout.module";
-import {uapiWidgetsModule} from "../shared/widgets/uapi-widgets.module";
-import {StatsModule} from "../shared/stats/stats.module";
-import {routing} from "./e-commerce.routing";
-import {uapiDatatableModule} from "../shared/ui/datatable/smartadmin-datatable.module";
-import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
-import {CarouselModule} from "ng2-bootstrap";
+import { uapiLayoutModule } from "../shared/layout/layout.module";
+import { uapiWidgetsModule } from "../shared/widgets/uapi-widgets.module";
+import { StatsModule } from "../shared/stats/stats.module";
+import { routing } from "./e-commerce.routing";
+import { uapiDatatableModule } from "../shared/ui/datatable/smartadmin-datatable.module";
+import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
+import { CarouselModule } from "ng2-bootstrap";
 
 @NgModule({
   imports: [
