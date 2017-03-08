b813a6271e89043bf2c104d2adbbf4a7b567c846
Jamin Quimby <jaminquimby@hotmail.com>
2017-03-07 23:32:59 -0500

    added MD5

--- src/app/shared/user/user.service.ts
+++ src/app/shared/user/user.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs/Rx';
 import { AngularFire } from 'angularfire2';
-import { FirebaseAuthState } from 'angularfire2';
 import { JsonApiService } from '../../core/api/json-api.service';
 import { UserInterface } from './user.interface';
-import { Md5 } from 'ts-md5';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Injectable()
 export class UserService {
 
-  constructor(public af: AngularFire, private fas: FirebaseAuthState) { }
+  constructor(public af: AngularFire) { }
   public getLoginInfo(): Observable<any> {
     return this.af.auth
       .map(user => user = user);
@@ -20,8 +19,8 @@ export class UserService {
     return this.af.database
       .object(`/users/${localStorage.getItem('uid')}/profile/`)
       .map(userDetail => {
-        let photoURL = { 'photoURL': '//gravatar.com/avatar/' + Md5(userDetail.photoURL) }
-        userDetail = Object.assign(userDetail, photoURL)
+        let photoURL = { 'photoURL': '//gravatar.com/avatar/' + Md5.hashStr(userDetail.photoURL) };
+        userDetail = Object.assign(userDetail, photoURL);
       });
     //      .take(1);
   }
